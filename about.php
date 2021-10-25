42bfd6a634927211180db81fa46f6b867e1a1638
Mhd. Arraza Pratama <68998357+mhdarrazaprtm@users.noreply.github.com>
2021-10-25 13:41:05 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>About | Nolife Reborn</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="css/tailwind/tailwind.css">
+    <link rel="stylesheet" href="assets/icons/css/all.min.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/icons/favicon-tailwind.png">
+    <style>
+       .banner {
+        background-color: #011133;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%233EFFFE' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%2301D3FE'%3E%3Ccircle cx='769' cy='229' r='5'/%3E%3Ccircle cx='539' cy='269' r='5'/%3E%3Ccircle cx='603' cy='493' r='5'/%3E%3Ccircle cx='731' cy='737' r='5'/%3E%3Ccircle cx='520' cy='660' r='5'/%3E%3Ccircle cx='309' cy='538' r='5'/%3E%3Ccircle cx='295' cy='764' r='5'/%3E%3Ccircle cx='40' cy='599' r='5'/%3E%3Ccircle cx='102' cy='382' r='5'/%3E%3Ccircle cx='127' cy='80' r='5'/%3E%3Ccircle cx='370' cy='105' r='5'/%3E%3Ccircle cx='578' cy='42' r='5'/%3E%3Ccircle cx='237' cy='261' r='5'/%3E%3Ccircle cx='390' cy='382' r='5'/%3E%3C/g%3E%3C/svg%3E");
+       }
+       .about {
+           color: #3efffe;
+           border-bottom: 4px solid #f80276;
+       }
+       .about-mobile {
+           background-color: #17375b;
+       }
+    </style>
+  </head>
+  <body class="antialiased bg-custom_blue text-white font-fixed">
+    <div class="flex flex-col h-screen">
+        <?php include("inc/header.php"); ?>
+        <section class="banner relative block border-b border-custom_blue3 border-opacity-60">
+            <div class="py-12 px-0 lg:py-20 lg:px-12.3 text-center">
+                <div class="container w-full px-3.7 mx-auto">
+                    <div class="flex flex-wrap justify-center -mx-3.7">
+                        <div class="w-full text-center">
+                            <h2 class="text-5xl font-semibold">ABOUT US</h2>
+                            <span class=""><a href="index.php" class="hover:underline hover:text-custom_blue2 hover:border-b-4 hover:border-custom_pink duration-200">Home</a> / <span class="text-custom_blue2 border-b-4 border-custom_pink">About</span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="py-12 lg:py-20 block">
+            <div class="container w-full px-3.7 mx-auto">
+                <div class="flex flex-wrap -mx-3.7">
+                    <div class="px-3 max-w-full relative w-full">
+                        <div class="mb-10">
+                            <li class="list-inside list-disc text-custom_blue2 text-2xl uppercase mb-2"><span class="text-white">About Us</span></li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores velit, incidunt repellendus quaerat earum cum tenetur iste consequuntur alias maiores facere similique iure aspernatur tempora illo et exercitationem nulla pariatur ad? Exercitationem fugit officiis nemo blanditiis natus praesentium dolorem maiores vitae, numquam facilis et nobis animi consectetur dignissimos velit expedita?</p>
+                        </div>
+                        <div class="mb-10">
+                            <li class="list-inside list-disc text-custom_blue2 text-2xl uppercase mb-2"><span class="text-white">Members</span></li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis fugit accusantium odio quis quae nobis, ullam aperiam assumenda nam minima in voluptatem ipsa, molestias ratione eius obcaecati expedita eaque ut minus nisi veniam inventore dolorem voluptatum? Cupiditate debitis numquam iusto ea quos voluptate molestias consequatur. Atque quas enim dignissimos quasi, reiciendis quod quis iusto cupiditate laborum ullam cum mollitia esse vitae sit debitis magni ex quo earum eius fugiat quae.</p>
+                        </div>
+                        <div class="">
+                            <li class="list-inside list-disc text-custom_blue2 text-2xl uppercase mb-2"><span class="text-white">Others</span></li>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, vel voluptatem temporibus numquam perferendis labore nisi libero reiciendis quod, rerum, mollitia iusto id enim nobis optio iure. Ducimus, voluptatum quasi.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php include("inc/footer.php"); ?>
+    </div>
+    <script src="js/main.js"></script>
+  </body>
+</html>
